2d5e73d23f01a07bba4277b16e2b8ce6ed25411b
Elias Norberg <elias@aisle.se>
2019-04-02 13:08:01 +0200

    do not create unverified context

--- juniper-vpn.py
+++ juniper-vpn.py
@@ -25,7 +25,7 @@ import datetime
 
 debug = False
 
-ssl._create_default_https_context = ssl._create_unverified_context
+#ssl._create_default_https_context = ssl._create_unverified_context
 
 """
 OATH code from https://github.com/bdauvergne/python-oath
